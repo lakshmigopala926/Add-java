b0575a92375ecf04d75e04084d39bcfc33e1ab7b
Gopala <varalakshmi.gopala@accenture.com>
2025-12-12 18:38:33 +0530

    Modify-Note

--- Add.java
+++ Add.java
@@ -1,5 +1,5 @@
 public class Add{
-	static void main(String[] args){
+	public static void main(String[] args){
 		int a=10;int b=20;
 		System.out.println("The sum is:" +(a+b));
 		System.out.println("The multiplication is:"+(a*b));
